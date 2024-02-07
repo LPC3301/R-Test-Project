8abf08d0d23a7b875afd7b406ed4d1af636d3378
LPC3301 <raffaeljonas.forrer@uzh.ch>
2024-02-07 10:30:42 +0100

    Added char-to-date conversion and a selection

--- test.R
+++ test.R
@@ -1,1 +1,13 @@
 #This is a file that will be pushed to GitHub in order to check if RStudio's Integration works or not
+
+#Now I will add some random code in order to check if commits work
+library("data.table")
+library("lubridate")
+library("dplyr")
+library("Hmisc")
+
+#Change the date from char to date
+transactions <- transactions[, TransDate:=as_datetime(TransDate, format="%d.%m.%Y")]
+
+#Select only the transactions later than 1975
+transact_shortened <- transactions[year(TransDate) > 1975]
